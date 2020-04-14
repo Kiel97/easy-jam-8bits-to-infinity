9ce8fb5531569d41f8564c330f6b99d7b2d4747c
Krzysztof Kiełczewski <kkielek97@gmail.com>
2020-04-14 13:44:39 +0200

    Game borders, Move movement to player, new movement mechanism

--- Game.gd
+++ Game.gd
@@ -1,28 +1,24 @@
 extends Node2D
 
-const DEBUG_ACC = false
+const DEBUG_ACC = true
 
-const ACC_ERROR_MARGIN = 0.25
-const ACC_MOVE_MULTIPLIER = 10
+const ACC_ERROR_MARGIN = 0.1
+const AXIS_Y_CORRECTION = -4
 
 onready var accXlabel: Label = $CanvasLayer/ACCValues/X
 onready var accYlabel: Label = $CanvasLayer/ACCValues/Y
-onready var accZlabel: Label = $CanvasLayer/ACCValues/Z
-
 
 func _ready():
 	accXlabel.visible = DEBUG_ACC
 	accYlabel.visible = DEBUG_ACC
 
-
-func _process(delta):
+func _process(_delta):
 	var acc: Vector3 = Input.get_accelerometer()
 	if (abs(acc.x) < ACC_ERROR_MARGIN):
 		acc.x = 0
 	if (abs(acc.y) < ACC_ERROR_MARGIN):
 		acc.y = 0
-	$Sprite.translate(Vector2(acc.x, -acc.y) * delta * ACC_MOVE_MULTIPLIER)
 	
 	if (DEBUG_ACC):
-		accXlabel.text = "X: " + str(stepify(acc.x, 0.001))
-		accYlabel.text = "Y: " + str(stepify(acc.y, 0.001))
+		accXlabel.text = "X: " + str(round(acc.x))
+		accYlabel.text = "Y: " + str(round(-acc.y))
