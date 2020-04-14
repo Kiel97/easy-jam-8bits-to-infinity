c1879e519f3a20dde172d0be6d4260f240ce4da5
Krzysztof Kiełczewski <kkielek97@gmail.com>
2020-04-14 14:00:52 +0200

    Removed acc margin, implemented accelerator Y axis correction

--- Game.gd
+++ Game.gd
@@ -3,7 +3,7 @@ extends Node2D
 const DEBUG_ACC = true
 
 const ACC_ERROR_MARGIN = 0.1
-const AXIS_Y_CORRECTION = -4
+const AXIS_Y_CORRECTION = 3
 
 onready var accXlabel: Label = $CanvasLayer/ACCValues/X
 onready var accYlabel: Label = $CanvasLayer/ACCValues/Y
@@ -14,11 +14,7 @@ func _ready():
 
 func _process(_delta):
 	var acc: Vector3 = Input.get_accelerometer()
-	if (abs(acc.x) < ACC_ERROR_MARGIN):
-		acc.x = 0
-	if (abs(acc.y) < ACC_ERROR_MARGIN):
-		acc.y = 0
 	
 	if (DEBUG_ACC):
 		accXlabel.text = "X: " + str(round(acc.x))
-		accYlabel.text = "Y: " + str(round(-acc.y))
+		accYlabel.text = "Y: " + str(round(-(acc.y + AXIS_Y_CORRECTION)))
--- Player.gd
+++ Player.gd
@@ -1,17 +1,13 @@
 extends KinematicBody2D
 
 const ACC_ERROR_MARGIN = 0.1
-const AXIS_Y_CORRECTION = -4
+const AXIS_Y_CORRECTION = 3
 
 var velocity
 
 func _process(_delta):
 	var acc: Vector3 = Input.get_accelerometer()
-	if (abs(acc.x) < ACC_ERROR_MARGIN):
-		acc.x = 0
-	if (abs(acc.y) < ACC_ERROR_MARGIN):
-		acc.y = 0
 	
-	velocity = Vector2(round(acc.x), round(-acc.y))
+	velocity = Vector2(round(acc.x), round(-(acc.y+AXIS_Y_CORRECTION)))
 	
 	velocity = move_and_collide(velocity.normalized() * sqrt(3))
