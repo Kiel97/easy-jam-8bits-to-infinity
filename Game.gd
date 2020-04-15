90e8e414e6ba8022aec2c00fe7494f55674a382a
Krzysztof Kiełczewski <kkielek97@gmail.com>
2020-04-16 01:52:35 +0200

    Show debug acc only on Android, disable it anyway

--- Game.gd
+++ Game.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const DEBUG_ACC = true
+const DEBUG_ACC = false
 
 const ACC_ERROR_MARGIN = 0.1
 const AXIS_Y_CORRECTION = 3
@@ -9,12 +9,12 @@ onready var accXlabel: Label = $CanvasLayer/ACCValues/X
 onready var accYlabel: Label = $CanvasLayer/ACCValues/Y
 
 func _ready():
-	accXlabel.visible = DEBUG_ACC
-	accYlabel.visible = DEBUG_ACC
+	accXlabel.visible = OS.get_name() == 'Android' and DEBUG_ACC
+	accYlabel.visible = OS.get_name() == 'Android' and DEBUG_ACC
 
 func _process(_delta):
 	var acc: Vector3 = Input.get_accelerometer()
 	
-	if (DEBUG_ACC):
+	if (OS.get_name() == 'Android' and DEBUG_ACC):
 		accXlabel.text = "X: " + str(round(acc.x))
 		accYlabel.text = "Y: " + str(round(-(acc.y + AXIS_Y_CORRECTION)))
