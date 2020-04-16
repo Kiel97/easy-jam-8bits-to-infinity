839614e131fe5c5c2dc0c6984a0c7640cca30ba6
Krzysztof Kiełczewski <kkielek97@gmail.com>
2020-04-16 19:46:13 +0200

    Reimplemented player movement with accelerometer

--- Player.gd
+++ Player.gd
@@ -1,36 +1,23 @@
 extends RigidBody2D
 
-const ACC_ERROR_MARGIN = 0.1
-const AXIS_Y_CORRECTION = 3
-const FOLLOW_SPEED = 1.2
+const MOUSE_FOLLOW_SPEED = 1.2
+const ACC_FOLLOW_SPEED = 22
+
+const ACC_Y_CORRECTION = 0
 
 var velocity
 
 onready var os: String = OS.get_name()
 
-#func _process(_delta):
-#
-#	if (os == 'Android' or os == 'iOS'):
-#		var acc: Vector3 = Input.get_accelerometer()
-#		velocity = Vector2(acc.x, -(acc.y+AXIS_Y_CORRECTION))
-#		velocity = move_and_collide(velocity.normalized() * sqrt(3))
-#	else: # Web
-#		var mouse_pos = get_global_mouse_position()
-#		var distance_x = (round(mouse_pos.x - self.position.x)) * FOLLOW_SPEED/100
-#		var distance_y = (round(mouse_pos.y - self.position.y)) * FOLLOW_SPEED/100
-#		velocity = Vector2(distance_x, distance_y)
-#
-#		velocity = move_and_collide(velocity)
 
 func _integrate_forces(state):
 	if (os == 'Android' or os == 'iOS'):
-		pass
-#		var acc: Vector3 = Input.get_accelerometer()
-#		add_force(Vector2(0, 0), Vector2(acc.x, -acc.y))
+		var acc: Vector3 = Input.get_accelerometer()
+		state.linear_velocity = Vector2(acc.x, -(acc.y+ACC_Y_CORRECTION)) * ACC_FOLLOW_SPEED
 	else:
 		var mouse_pos = get_global_mouse_position()
 		var distance_x = (round(mouse_pos.x - self.position.x))
 		var distance_y = (round(mouse_pos.y - self.position.y))
 		velocity = Vector2(distance_x, distance_y)
 		
-		state.linear_velocity = velocity * FOLLOW_SPEED
+		state.linear_velocity = velocity * MOUSE_FOLLOW_SPEED
