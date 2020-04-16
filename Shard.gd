6d058aaaa528c984d4c082fc2970bdf2277c7a98
Krzysztof Kiełczewski <kkielek97@gmail.com>
2020-04-16 21:53:14 +0200

    Added dormant float for shards when there were no collision with player

--- Shard.gd
+++ Shard.gd
@@ -4,6 +4,18 @@ export var red: bool = true setget set_red
 export var green: bool = true setget set_green
 export var blue: bool = true setget set_blue
 
+var dormant_velocity: Vector2 = Vector2(10, 10)
+var dormant = true
+
+func _integrate_forces(_state):
+	if(dormant):
+		self.linear_velocity = dormant_velocity
+
+func _on_Shard_body_entered(body: Node2D):
+	if body.is_in_group("player") and dormant:
+		dormant = false
+		print("+1")
+
 func set_red(value):
 	red = value
 	call_deferred("update_color")
@@ -18,3 +30,5 @@ func set_blue(value):
 
 func update_color():
 	$Polygon2D.modulate = Color(float(red), float(green), float(blue))
+
+
