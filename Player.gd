3e3fec8bee4b9c2839a1b6df98a559efc85e82da
Krzysztof Kiełczewski <kkielek97@gmail.com>
2020-04-16 19:49:40 +0200

    Reimplemented player movement
    * Player node changed to RB2D, implemented movement with mouse
    
    * Reimplemented player movement with accelerometer
    
    * Enlarge level border colliders

--- Player.gd
+++ Player.gd
@@ -1,23 +1,23 @@
-extends KinematicBody2D
+extends RigidBody2D
 
-const ACC_ERROR_MARGIN = 0.1
-const AXIS_Y_CORRECTION = 3
-const FOLLOW_SPEED = 0.08
+const MOUSE_FOLLOW_SPEED = 1.2
+const ACC_FOLLOW_SPEED = 22
+
+const ACC_Y_CORRECTION = 0
 
 var velocity
 
 onready var os: String = OS.get_name()
 
-func _process(_delta):
 
+func _integrate_forces(state):
 	if (os == 'Android' or os == 'iOS'):
 		var acc: Vector3 = Input.get_accelerometer()
-		velocity = Vector2(acc.x, -(acc.y+AXIS_Y_CORRECTION))
-		velocity = move_and_collide(velocity.normalized() * sqrt(3))
-	else: # Web
+		state.linear_velocity = Vector2(acc.x, -(acc.y+ACC_Y_CORRECTION)) * ACC_FOLLOW_SPEED
+	else:
 		var mouse_pos = get_global_mouse_position()
-		var distance_x = (round(mouse_pos.x - self.position.x)) * FOLLOW_SPEED/100
-		var distance_y = (round(mouse_pos.y - self.position.y)) * FOLLOW_SPEED/100
+		var distance_x = (round(mouse_pos.x - self.position.x))
+		var distance_y = (round(mouse_pos.y - self.position.y))
 		velocity = Vector2(distance_x, distance_y)
-
-		velocity = move_and_collide(velocity)
+		
+		state.linear_velocity = velocity * MOUSE_FOLLOW_SPEED
