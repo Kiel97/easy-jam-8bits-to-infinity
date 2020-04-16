12b5b61c16e74dd7af54fd6677039a11d9920cfe
Krzysztof Kiełczewski <kkielek97@gmail.com>
2020-04-16 21:26:26 +0200

    And so does Shard

--- Shard.gd
+++ Shard.gd
@@ -1,16 +1,20 @@
 extends RigidBody2D
 
+export var red: bool = true setget set_red
+export var green: bool = true setget set_green
+export var blue: bool = true setget set_blue
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func set_red(value):
+	red = value
+	call_deferred("update_color")
 
+func set_green(value):
+	green = value
+	call_deferred("update_color")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func set_blue(value):
+	blue = value
+	call_deferred("update_color")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func update_color():
+	$Polygon2D.modulate = Color(float(red), float(green), float(blue))
