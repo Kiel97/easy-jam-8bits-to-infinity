8c743de2d85efed9ce8a2546cc7c1262da498915
Krzysztof Kiełczewski <kkielek97@gmail.com>
2020-05-30 17:14:22 +0200

    Refactor Player.gd

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,50 +1,56 @@
+class_name Player
 extends RigidBody2D
 
-const MOUSE_FOLLOW_SPEED = 2.4
-const ACC_FOLLOW_SPEED = 24
+const MOUSE_FOLLOW_SPEED: float = 2.4
+const ACC_FOLLOW_SPEED: float = 24.0
 
-var acc_y_correction = 0
+export var red: bool = true setget _set_red
+export var green: bool = true setget _set_green
+export var blue: bool = true setget _set_blue
 
-var during_game = false
-var velocity
+var during_game: bool = false
 
-export var red: bool = true setget set_red
-export var green: bool = true setget set_green
-export var blue: bool = true setget set_blue
+var _velocity: Vector2
+var _acc_y_correction: float = 0.0
 
 onready var os: String = OS.get_name()
+onready var sprite := $Sprite as Sprite
+onready var bounce_sfx := $BounceSound as AudioStreamPlayer
 
-func _integrate_forces(state):
+func _integrate_forces(state: Physics2DDirectBodyState):
 	if (during_game):
-		if (os == 'Android' or os == 'iOS'):
+		if _is_mobile_device():
 			var acc: Vector3 = Input.get_accelerometer()
 			state.linear_velocity = Vector2(acc.x, -(acc.y)) * ACC_FOLLOW_SPEED
-			#state.linear_velocity.y -= acc_y_correction FIXME
+			state.linear_velocity.y -= _acc_y_correction
 		else:
-			var mouse_pos = get_global_mouse_position()
-			var distance_x = (round(mouse_pos.x - self.position.x))
-			var distance_y = (round(mouse_pos.y - self.position.y))
-			velocity = Vector2(distance_x, distance_y)
+			var mouse_pos: Vector2 = get_global_mouse_position()
+			var distance_x: float = (round(mouse_pos.x - self.position.x))
+			var distance_y: float = (round(mouse_pos.y - self.position.y))
 			
-			state.linear_velocity = velocity * MOUSE_FOLLOW_SPEED
+			_velocity = Vector2(distance_x, distance_y)
+			state.linear_velocity = _velocity * MOUSE_FOLLOW_SPEED
 	else:
 		state.linear_velocity = Vector2.ZERO
+
+func _is_mobile_device():
+	return os in ['Android', 'iOS']
 		
-func set_red(value):
+func _set_red(value: bool):
 	red = value
-	call_deferred("update_color")
+	call_deferred("_update_color")
 
-func set_green(value):
+func _set_green(value: bool):
 	green = value
-	call_deferred("update_color")
+	call_deferred("_update_color")
 
-func set_blue(value):
+func _set_blue(value: bool):
 	blue = value
-	call_deferred("update_color")
+	call_deferred("_update_color")
 
-func update_color():
-	$Sprite.modulate = Color(float(red), float(green), float(blue))
+func _update_color():
+	sprite.modulate = Color(float(red), float(green), float(blue))
 
-func _on_Player_body_entered(body):
+func _on_Player_body_entered(_body: Node2D):
 	if (during_game):
-		$BounceSound.play()
+		bounce_sfx.play()
