0333ce1e784ebc0c266360c24234ac1bdbbaf3d5
Krzysztof Kiełczewski <kkielek97@gmail.com>
2020-05-30 17:58:08 +0200

    Refactor Game.gd, part 1 (naming conventions and lines order)

--- Scenes/Game/Game.gd
+++ Scenes/Game/Game.gd
@@ -1,167 +1,187 @@
 extends Node2D
 
-var score: int = 0 setget set_score
-var is_playing: bool = false setget playing
-
-var highscore: int = 0
-var highscore_path: String = "user://gchiscore.txt"
-var highscore_file: File = File.new()
-
-onready var spawn_point: PathFollow2D = $JunkGenerator/JunkPath/JunkPathSpawner
-onready var junk_timer: Timer = $JunkGenerator/JunkTimer
-onready var score_timer: Timer = $CanvasLayer/ScoreGameLabel/ScoreLabelTimer
-
-onready var main_overlay: Control = $CanvasLayer/MainOverlay
-onready var tap_button: TextureButton = $CanvasLayer/MainOverlay/TapButton
-onready var tap_label: Label = $CanvasLayer/MainOverlay/MidVBox/TapLabel
-onready var title_label: Label = $CanvasLayer/MainOverlay/TopVBox/TitleLabel
-onready var score_label: Label = $CanvasLayer/MainOverlay/TopVBox/ScoreLabel
-onready var hiscore_label: Label = $CanvasLayer/MainOverlay/TopVBox/HighscoreLabel
-onready var blink_timer: Timer = $CanvasLayer/MainOverlay/TopVBox/HighscoreLabel/OneBlinkTimer
-
-onready var credits_overlay: Control = $CanvasLayer/CreditsOverlay
-
-onready var score_game_label: Label = $CanvasLayer/ScoreGameLabel
-
 export var junks: Array = [load("res://Scenes/Junk/JunkDiamond.tscn")]
+
+var _score: int = 0 setget _set_score
+var _is_playing: bool = false setget _set_playing_state
+
+var _blinks_new_score: int = 6
+var _highscore: int = 0
+var _highscore_file: File = File.new()
+var _highscore_path: String = "user://gchiscore.txt"
+
+var _score_string: String = "Score: %d"
+var _highscore_string: String = "High: %d"
+
+# Timers
+onready var junk_timer := $JunkGenerator/JunkTimer as Timer
+onready var score_timer := $CanvasLayer/ScoreGameLabel/ScoreLabelTimer as Timer 
+onready var blink_timer := $CanvasLayer/MainOverlay/TopVBox/HighscoreLabel/OneBlinkTimer as Timer
+
+# UI
+onready var main_overlay := $CanvasLayer/MainOverlay as Control
+onready var tap_button := $CanvasLayer/MainOverlay/TapButton as TextureButton
+onready var tap_label := $CanvasLayer/MainOverlay/MidVBox/TapLabel as Label
+onready var title_label := $CanvasLayer/MainOverlay/TopVBox/TitleLabel as Label
+onready var score_label := $CanvasLayer/MainOverlay/TopVBox/ScoreLabel as Label
+onready var hiscore_label := $CanvasLayer/MainOverlay/TopVBox/HighscoreLabel as Label
+onready var credits_overlay := $CanvasLayer/CreditsOverlay as Control
+onready var score_game_label := $CanvasLayer/ScoreGameLabel as Label
+
+# Sound effects
+onready var music := $Music as AudioStreamPlayer
+onready var menu_click_sfx := $MenuClickSound as AudioStreamPlayer
+onready var collected_sfx := $CollectedSound as AudioStreamPlayer
+onready var lose_sfx := $LoseSound as AudioStreamPlayer
+onready var new_highscore_sfx := $NewHighscoreSound as AudioStreamPlayer
+
+# Other
+onready var spawn_point := $JunkGenerator/JunkPath/JunkPathSpawner as PathFollow2D
+onready var player := $Player as Player
+onready var junk_container := $Junk as Node2D
+onready var bin_container := $Bins as Node2D
 
 func _ready():
 	randomize()
 	score_game_label.visible = false
 	credits_overlay.visible = false
 	main_overlay.visible = true
-	self.is_playing = false
-	load_highscore()
+	self._is_playing = false
+	_load_highscore()
 	assert(junks.size() >= 1)
-	$Music.play()
+	music.play()
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	elif Input.is_mouse_button_pressed(BUTTON_LEFT) and _is_playing:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
 
-func _on_JunkTimer_timeout():
-	spawn_random_junk()
-
-func playing(value):
-	is_playing = value
-	if not is_playing:
+func _set_playing_state(value: bool):
+	_is_playing = value
+	if not _is_playing:
 		junk_timer.autostart = false
-		#$Player.visible = false
 		junk_timer.stop()
-		$Player.during_game = false
+		player.during_game = false
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		for bin in $Bins.get_children():
-			bin.active = false
+		_set_bins_status(false)
 	else:
 		junk_timer.autostart = true
-		#$Player.visible = true
 		junk_timer.start()
-		$Player.during_game = true
+		player.during_game = true
 		Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
-		for bin in $Bins.get_children():
-			bin.active = true
+		_set_bins_status(true)
 
-func new_game():
-	self.is_playing = true
+func _new_game():
+	for junk in junk_container.get_children():
+		junk.queue_free()
+	self._score = 0
 	main_overlay.visible = false
-	for child in $Junk.get_children():
-		child.queue_free()
-	self.score = 0
-	get_viewport().warp_mouse($Player.position)
-	
-	if OS.get_name() == "Android" or OS.get_name() == "iOS":
-		var y_correction := Input.get_accelerometer().y
-		$Player.acc_y_correction = -y_correction
+	get_viewport().warp_mouse(player.position)
+	self._is_playing = true
 
-func set_score(value):
-	score = value
-	score_game_label.text = str(score)
-	if is_playing:
-		show_score()
-
-func show_score():
-	score_game_label.visible = true
-	score_timer.start()
-
-func hide_score():
-	score_game_label.visible = false
-
-func spawn_random_junk():
+func _spawn_random_junk():
 	spawn_point.offset = randi()
-	var junk = junks[randi() % junks.size()].instance()
-	$Junk.add_child(junk)
-	var direction = spawn_point.rotation + PI / 2
-	junk.position = spawn_point.position
+	var junk := junks[randi() % junks.size()].instance() as Junk
+	junk_container.add_child(junk)
+	
+	var direction: float = spawn_point.rotation + PI / 2
 	direction += rand_range(-PI / 4, PI / 4)
+	
+	junk.position = spawn_point.position
 	junk.rotation = direction
 	junk.randomize_color()
 
-func game_over():
-	self.is_playing = false
+func _game_over():
+	self._is_playing = false
 	main_overlay.visible = true
-	update_highscore()
+	_update_highscore()
 	
-	for junk in $Junk.get_children():
+	for junk in junk_container.get_children():
 		junk.during_game = false
 	
-	score_label.text = "Score: " + str(score)
+	score_label.text = _score_string % _score
 
-func load_highscore():
-	if not highscore_file.file_exists(highscore_path):
-		highscore = 0
-		highscore_file.open(highscore_path, File.WRITE)
-		highscore_file.store_string(str(highscore))
-		highscore_file.close()
+func _set_score(value: int):
+	_score = value
+	score_game_label.text = str(_score)
+	if _is_playing:
+		_show_score()
+
+func _show_score():
+	score_game_label.visible = true
+	score_timer.start()
+
+func _hide_score():
+	score_game_label.visible = false
+
+func _load_highscore():
+	if not _highscore_file.file_exists(_highscore_path):
+		_highscore = 0
+		_highscore_file.open(_highscore_path, File.WRITE)
+		_highscore_file.store_string(str(_highscore))
+		_highscore_file.close()
 	
-	highscore_file.open(highscore_path, File.READ)
-	highscore = int(highscore_file.get_as_text())
-	highscore_file.close()
-	hiscore_label.text = "High: " + str(highscore)
+	_highscore_file.open(_highscore_path, File.READ)
+	_highscore = int(_highscore_file.get_as_text())
+	_highscore_file.close()
+	
+	hiscore_label.text = _highscore_string % _highscore
 
-func update_highscore():
-	if score > highscore:
-		highscore = score
-		highscore_file.open(highscore_path, File.WRITE)
-		highscore_file.store_string(str(highscore))
-		highscore_file.close()
-		$NewHighscoreSound.play()
-		blink_highscore_label()
+func _update_highscore():
+	if _score > _highscore:
+		_highscore = _score
+		
+		_highscore_file.open(_highscore_path, File.WRITE)
+		_highscore_file.store_string(str(_highscore))
+		_highscore_file.close()
+		
+		new_highscore_sfx.play()
+		_blink_highscore_label()
 	else:
-		$LoseSound.play()
-	hiscore_label.text = "High: " + str(highscore)
+		lose_sfx.play()
+	
+	hiscore_label.text = _highscore_string % _highscore
 
-func _on_TapButton_pressed():
-	$MenuClickSound.play()
-	new_game()
+func _set_bins_status(status: bool):
+	for bin in bin_container.get_children():
+		bin.active = status
 
-func _on_Bin_junk_collected():
-	self.score += 1
-	$CollectedSound.play()
-
-func _on_Bin_wrong_color():
-	game_over()
-
-func _on_CreditsButton_pressed():
-	$MenuClickSound.play()
-	main_overlay.visible = false
-	credits_overlay.visible = true
-
-func _on_ScoreLabelTimer_timeout():
-	hide_score()
-
-func _on_TapCreditsButton_pressed():
-	$MenuClickSound.play()
-	credits_overlay.visible = false
-	main_overlay.visible = true
-
-func blink_highscore_label():
-	for i in range(6):
+func _blink_highscore_label():
+	for i in range(_blinks_new_score):
 		hiscore_label.visible = not hiscore_label.visible
 		blink_timer.start()
 		yield(blink_timer, "timeout")
 
+func _on_Bin_junk_collected():
+	self._score += 1
+	collected_sfx.play()
+
+func _on_Bin_wrong_color():
+	_game_over()
+
+func _on_CreditsButton_pressed():
+	menu_click_sfx.play()
+	main_overlay.visible = false
+	credits_overlay.visible = true
+
+func _on_JunkTimer_timeout():
+	_spawn_random_junk()
+
 func _on_MusicButton_pressed():
-	if $Music.playing:
-		$Music.stop()
+	if music.playing:
+		music.stop()
 	else:
-		$Music.play()
+		music.play()
+
+func _on_ScoreLabelTimer_timeout():
+	_hide_score()
+
+func _on_TapButton_pressed():
+	menu_click_sfx.play()
+	_new_game()
+
+func _on_TapCreditsButton_pressed():
+	menu_click_sfx.play()
+	credits_overlay.visible = false
+	main_overlay.visible = true
