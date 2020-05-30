b185f284c430d23cefda2351c2c52bcf24b95231
Krzysztof Kiełczewski <kkielek97@gmail.com>
2020-05-30 15:32:26 +0200

    Refactor Bin.gd (except not safe signal declaration lines)

--- Scenes/Bin/Bin.gd
+++ Scenes/Bin/Bin.gd
@@ -1,3 +1,4 @@
+class_name Bin
 extends Area2D
 
 signal junk_collected
@@ -8,6 +9,11 @@ export var green: bool = true setget set_green
 export var blue: bool = true setget set_blue
 
 var active: bool = true
+
+onready var sprite := $Sprite as Sprite
+
+func update_color():
+	sprite.modulate = Color(float(red), float(green), float(blue))
 
 func set_red(value: bool):
 	red = value
@@ -21,14 +27,13 @@ func set_blue(value: bool):
 	blue = value
 	call_deferred("update_color")
 
-func update_color():
-	$Sprite.modulate = Color(float(red), float(green), float(blue))
-
 func _on_Bin_body_entered(body: Node2D):
 	if active:
-		if body.is_in_group("junk") and not body.dormant:
-			if body.get_color() == $Sprite.modulate:
-				emit_signal("junk_collected")
-			else:
-				emit_signal("wrong_color")
-			body.queue_free()
+		if body.is_in_group("junk"):
+			var junk_body := body as Junk
+			if not junk_body.dormant:
+				if junk_body.get_color() == sprite.modulate:
+					emit_signal("junk_collected")
+				else:
+					emit_signal("wrong_color")
+				junk_body.queue_free()
